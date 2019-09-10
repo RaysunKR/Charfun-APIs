d1788a4e5207c12322e67a36bfb8bff6571534dc
RaysunKR <raysunkr@qq.com>
2019-09-10 16:05:46 +0800

    no message

--- Demos/Python/shorturl.py
+++ Demos/Python/shorturl.py
@@ -4,10 +4,11 @@ import re
 
 
 def shortUrlGen(longurl="https://www.charfun.com/"):
-    # pattern=r'/^((http|ftp|https):\/\/)?[\w-_.]+(\/[\w-_]+)*\/?$/'
-    if re.match(r'(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))',longurl):
+    pattern = r'(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))'
+    if re.match(pattern, longurl):
         http = urllib3.PoolManager()
-        r = http.request('POST', "https://www.charfun.com/api/shorturl",fields={"longurl":longurl})
+        r = http.request(
+            'POST', "https://www.charfun.com/api/shorturl", fields={"longurl": longurl})
         rawData = r.data.decode()
         data = json.loads(rawData)
 
@@ -16,7 +17,6 @@ def shortUrlGen(longurl="https://www.charfun.com/"):
         print("Illegal URL.")
         return False
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     print(shortUrlGen("https://www.charfun.com/"))
-    
-    
